4f0c58f9144c9f509b005ceb08d4b4dfc9034edd
charlotte Biney <charlotte.biney5@gmail.com>
2017-10-22 07:36:23 +0100

    added few lines of code

---
+++ src/main/java/web01project/App.java
@@ -0,0 +1,13 @@
+package web01project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
